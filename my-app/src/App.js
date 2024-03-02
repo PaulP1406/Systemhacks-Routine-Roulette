3a14b7d3649072a9647d9ae39f4a1ecb5327114d
Duc Duy Pham <ddp4@sfu.ca>
2024-03-02 11:45:32 -0800

    added nav

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Schedule from './Schedule';
 import ChaosButtonGlobal from './ChaosButtonGlobal';
+import Nav from './Nav';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Schedule />
       <ChaosButtonGlobal />
     </div>
---
+++ my-app/src/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className='Nav'>Nav
+        
+    </div>
+  )
+}
+
+export default Nav
