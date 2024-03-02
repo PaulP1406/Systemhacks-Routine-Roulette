b097ab59c0d1ab7ef91b680c59ff07176c671253
Duc Duy Pham <ddp4@sfu.ca>
2024-03-02 12:25:37 -0800

    Modified Schedule.js

--- my-app/src/Schedule.js
+++ my-app/src/Schedule.js
@@ -6,12 +6,22 @@ const ScheduleComponent = () => {
   return (
     <div className="Schedule">
       <TaskTitle title="MORNING" />
-      <TaskBar name="Morning Task" />
+      <TaskBar name="wake up" />
+      <TaskBar name="brush teeth" />
+      <TaskBar name="brush teeth" />
+
       {/* Repeat for Afternoon and Evening */}
       <TaskTitle title="AFTERNOON" />
-      <TaskBar name="Afternoon Task" />
+      <TaskBar name="Coffee" />
+      <TaskBar name="Work" />
+      <TaskBar name="Coffee" />
+
       <TaskTitle title="EVENING" />
-      <TaskBar name="Evening Task" />
+      <TaskBar name="Go home" />
+      <TaskBar name="gym" />
+      <TaskBar name="Sleep" />    
+      
+
     </div>
   );
 };
